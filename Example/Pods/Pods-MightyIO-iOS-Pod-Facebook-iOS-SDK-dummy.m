94d6fbc87717c6c0b588e37140046a64bf4065dd
Gavin Potts <gavin@facultycreative.com>
2014-07-10 11:53:46 -0400

    Added necessary dependencies Updated to latest version of iOS SDK

---
+++ Example/Pods/Pods-MightyIO-iOS-Pod-Facebook-iOS-SDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MightyIO_iOS_Pod_Facebook_iOS_SDK : NSObject
+@end
+@implementation PodsDummy_Pods_MightyIO_iOS_Pod_Facebook_iOS_SDK
+@end
